a08fb25bf7d788b03da24e5293ced2cc72e7c282
Anton Shever <anton.shever96@gmail.com>
2024-11-15 16:34:22 +0200

    develop

--- app/Services/SegmentationService.php
+++ app/Services/SegmentationService.php
@@ -16,14 +16,14 @@ class SegmentationService
         if (blank($task->files['original'])) {
             return;
         }
-        // dd(route('api.webhook.processing', $task->id));
 
-        $response = Http::post('http://192.168.0.100:5310/api/process', [
+        $domain = env('AI_MODEL_ENDPOINT');
+        $response = Http::post("{$domain}/process", [
             'task_id' => $task->id,
             'image_url' => $task->files['original'], // s3/spaces
             'cleaning_model' => $task->cleaning_model,
-            // 'webhook_url' => route('api.webhook.processing', $task->id),
-            'webhook_url' => 'http://127.0.0.1:8000/api/webhook/processing/' . $task->id,
+            'webhook_url' => route('api.webhook.processing', $task->id),
+            // 'webhook_url' => 'http://127.0.0.1:8000/api/webhook/processing/' . $task->id,
         ]);
 
         if ($response->ok()) {
